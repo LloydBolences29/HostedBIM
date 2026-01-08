b70608dc828e715f1b5b3ece4da34516daf1c0ef
LloydBolences29 <lmbolences@gmail.com>
2026-01-08 19:06:37 +0800

    added the ssl

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,22 +1,26 @@
-const mysql2 = require('mysql2/promise');
-require('dotenv').config();
+const mysql2 = require("mysql2/promise");
+require("dotenv").config();
 
-let connection
+let connection;
 
-const connectToDatabase = async () =>{
-    if(!connection){
-        connection =  await mysql2.createPool({
-            host: process.env.MYSQL_HOST,
-            user: process.env.MYSQL_USER,
-            port: process.env.MYSQL_PORT,
-            password: process.env.MYSQL_PASSWORD,
-            database: process.env.MYSQL_DB,
-            waitForConnections: true,
-            connectionLimit: 10,
-            queueLimit: 0
-        })
-    }
-    return connection;
-}
+const connectToDatabase = async () => {
+  if (!connection) {
+    connection = await mysql2.createPool({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      port: process.env.MYSQL_PORT,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+      ssl: {
+        minVersion: "TLSv1.2",
+        rejectUnauthorized: true,
+      },
+    });
+  }
+  return connection;
+};
 
-module.exports = {connectToDatabase};
+module.exports = { connectToDatabase };
